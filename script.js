689b36dc3392db9bf65eb6e628dd7c3341446d0e
Mike Orsow <mike.orsow@gmail.com>
2022-07-24 23:07:57 -0700

    Make first letter of each choice uppercase

--- script.js
+++ script.js
@@ -1,27 +1,31 @@
 const getRandomIntBetween = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
 const getComputerChoice = () => ["rock", "paper", "scissors"][getRandomIntBetween(0,2)];
+const makeFirstCharUpper = (string) => string.slice(0,1).toUpperCase() + string.slice(1);
 
-const userChoice = prompt("Choose Rock, Paper, or Scissors").toLocaleLowerCase();
+// Prompt user for their choice. Store answer in userChoice.
+const userChoice = makeFirstCharUpper(prompt("Choose Rock, Paper, or Scissors").toLowerCase());
 console.log(`userChoice is ${userChoice}`);
-const computerChoice = getComputerChoice();
+
+// Get the computer's choice. Store answer in computerChoice.
+const computerChoice = makeFirstCharUpper(getComputerChoice());
 console.log(`computerChoice is ${computerChoice}`);
 
-// test auto-generated computerChoice, hardcoded userChoice
+// test auto-generated computerChoice, prompted userChoice
 let playRound = (userChoice, computerChoice) => {
-    const winningOutcomes = ["paperrock", "scissorspaper", "rockscissors"];
-    const roundChoices = `${userChoice}${computerChoice}`;
+    const winningChoicesArr = ["paperrock", "scissorspaper", "rockscissors"];
+    console.log(`roundChoices before lower: ${userChoice}${computerChoice}`)
+    const roundChoices = `${userChoice}${computerChoice}`.toLowerCase();
+    console.log(`roundChoices after lower: ${roundChoices}`)
     if (computerChoice == userChoice) {
         return `${userChoice} vs ${computerChoice}... it's a tie!`
     }
     else { 
-        return winningOutcomes.includes(roundChoices) ? `You win! ${userChoice} beats ${computerChoice}!` : `You lose! ${computerChoice} beats ${userChoice}!`;
+        return winningChoicesArr.includes(roundChoices) ? `You win! ${userChoice} beats ${computerChoice}!` : `You lose! ${computerChoice} beats ${userChoice}!`;
     }
 }
 
-
 console.log(playRound(userChoice, computerChoice))
 
-// Prompt user for their choice. Store answer in userChoice
 // Check if 
 
 // turn all of the above into a function, playRound that accepts 2 parameters computerChoice & userChoice
