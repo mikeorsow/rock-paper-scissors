76c8282d9f0857d3a40bf2531da8205a63c1321d
Mike Orsow <mike.orsow@gmail.com>
2022-07-25 23:06:10 -0700

    Refactor and move more actions into functions

--- script.js
+++ script.js
@@ -1,129 +1,132 @@
-
 // Set number of times to play the game
 const roundsToPlay = getRoundsToPlay();
-console.log(`roundsToPlay value: ${roundsToPlay}`);
-
-const userWantsToPlay = !(roundsToPlay === 0);
-console.log(`userWantsToPlay value: ${userWantsToPlay}`);
 
 // Check if the user wants to play the game
-if (userWantsToPlay){
+if (roundsToPlay > 0){
     runGame(roundsToPlay)   
 } 
 else {
     console.log(`Got it! No Rock, Paper, Scissors for you today!`);
 }
 
+// Run the game!
 function runGame(roundsToPlay) {
 
-    const winningChoicesArr = ["paperrock", "scissorspaper", "rockscissors"];
-    
     // Initialize Scores
     let userScore = 0;
     let computerScore = 0;
 
-  
+    // Play 'roundsToPlay' number of rounds
     for (let i = 0; i < roundsToPlay; i++) {
-        playRound();
-    }    
 
-    // Play a round of Rock Paper Scissors
-    function playRound() {
+        // Prompt user for their choice.
+        const promptInput = prompt("Choose Rock, Paper, or Scissors");
+       
+        // Check if the user hit cancel
+        if (promptInput === null) {
+            return console.log(`Game exited!`)
+        }
+
+        // make user choice first letter uppercase, the rest lowercase
+        const userChoice = makeFirstCharUpper(promptInput)
+
+        // Get the computer's choice.
+        const randomInt = getRandomIntBetween(0, 2);
+        const computerChoice = ["Rock", "Paper", "Scissors"][randomInt];
         
-        // const playersChoices = getPlayersChoices();
-        const userChoice = getUserChoice();
-        const computerChoice = getComputerChoice();
+        // Store current round number to display in round result message
+        const roundNumber = i+1;
 
-        getRoundResult(userChoice, computerChoice);
+        // Check for a tie
+        if (userChoice == computerChoice) {
+            console.log(tieMessage())
+        }
+        // If it wasn't a tie, calculate the winner of the round
+        else {
 
-        function getUserChoice() {
-            // Prompt user for their choice. Store answer in userChoice.
-            const userChoice = prompt("Choose Rock, Paper, or Scissors").toLowerCase();
-            // print the user choice
-            console.log(`userChoice is ${userChoice}`);
-            return userChoice;
+            // See if the user or the computer won. 
+            const roundWinner = getRoundWinner(userChoice, computerChoice);
+
+            // Increment the winner's score
+            roundWinner == "user" ? userScore++ : computerScore++
+
+            // Display the round result
+            console.log(getResultMessage(roundWinner));
         }
 
-        function getComputerChoice() {
-            // Get the computer's choice
-            const randomInt = getRandomIntBetween(0, 2);
-            const computerChoice = ["rock", "paper", "scissors"][randomInt];
-            // print the computer choice
-            console.log(`computerChoice is ${computerChoice}`);
-            return computerChoice;
-        }
+        // ----- FUNCTIONS ----- //
 
-        function getRoundResult(userChoice, computerChoice) {
+        // Returns a string that tells the user if they won or lost the round
+        function getResultMessage(roundWinner) {
+            
+            return roundWinner == "user" ? winMessage() : loseMessage();
 
-            const roundChoices = `${userChoice}${computerChoice}`.toLowerCase();
-
-            if (computerChoice == userChoice) {
-                return `${userChoice} vs ${computerChoice}... it's a tie!`;
+            function winMessage() {
+                return `You won round ${roundNumber}! ${userChoice} beats ${computerChoice}!`;
             }
-            else if (winningChoicesArr.includes(roundChoices)) {
-                userScore++;
-                return `You win! ${userChoice} beats ${computerChoice}!`;
-            }
-            else {
-                computerScore++;
-                return `You lose! ${computerChoice} beats ${userChoice}!`;
+    
+            function loseMessage() {
+                return `You lost round ${roundNumber}! ${computerChoice} beats ${userChoice}!`;
             }
         }
 
-       // console.log(getPlayerChoices(userChoice, computerChoice));
-        //console.log(`Your score: ${userScore}. CPU score: ${computerScore}`);
+        // Returns a string that tells the user it was a tie
+        function tieMessage() {
+            return `${userChoice} vs ${computerChoice}... round ${roundNumber} is a tie!`
+        }    
+
+        // returns the winner as either 'user', or 'computer'
+        function getRoundWinner(userChoice, computerChoice) {
+           
+            const winningCombosArr = ["paperrock", "scissorspaper", "rockscissors"];
+            const currentRoundCombo = getRoundCombo(userChoice, computerChoice);
+           
+            return winningCombosArr.includes(currentRoundCombo) ? "user" : "computer";
+    
+            function getRoundCombo(choice1, choice2) {
+                return `${choice1}${choice2}`.toLowerCase();
+            }
+        }    
     }
 
+    // Tell the user if they won, lost, or tied the game
+    console.log(getFinalScores());
 
-    // TO-DO: Turn this section into a function that accepts userScore and computerScore 
-    if (userScore == computerScore) {
-        return console.log(
-            `It's a tie!!!!! Final Score: You: ${userScore}. CPU: ${computerScore}.`
-        );
-    }
-    else if (userScore > computerScore) {
-        return console.log(
-            `You Win!!!!! Final Score: You: ${userScore}. CPU: ${computerScore}.`
-        );
-    }
-    else {
-        return console.log(
-            `You Lose!!!!! Final Score: You: ${userScore}. CPU: ${computerScore}.`
-        );
+    // Returns a string with the final score of the game
+    function getFinalScores() {
+
+        if (userScore == computerScore) {
+            return `The game was a tie!!!!! Final Score: You: ${userScore}. CPU: ${computerScore}.`
+        }
+        else if (userScore > computerScore) {
+            return `You won the game!!!!! Final Score: You: ${userScore}. CPU: ${computerScore}.`
+        }
+        else {
+            return `You lost the game!!!!! Final Score: You: ${userScore}. CPU: ${computerScore}.`
+        }
     }
 };
 
+// Prompt the user to see how many times they want to play.
+// Returns an int, or 0 if the user enters a string, empty string, or hits cancel.
+function getRoundsToPlay() {
 
+    let promptInput = prompt("How many times would you like to play Rock Paper Scissors?", 5);
+    
+    // handle cancel
+    if (promptInput === null) {
+        return promptInput = 0;
+    }
+    // convert the input into an int
+    else {
+        return parseInt(promptInput)
+    }
+}
 
 function getRandomIntBetween(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-
-
 function makeFirstCharUpper(string) {
-    return string.slice(0, 1).toUpperCase() + string.slice(1);
-}
-
-// function userWantsToPlay() {
-//     return (!roundsToPlay === 0)
-// }
-
-function getRoundsToPlay() {
-    console.log(`getRoundsToPlay() ran`)
-    let promptInput = prompt("How many times would you like to play Rock Paper Scissors?", 5);
-    console.log(`promptInput = ${promptInput}`)
-    // handle cancel
-    if (promptInput === null) {
-        return promptInput = 0;
-    }
-    else {
-        return parseInt(promptInput)
-    }
-    // handle empty string (user)
-    // handle 0
-    // handle if negative or over 10 isReasonableNumber lol
-      // Cancel == null
-    // Empty OK == ""
-    // parseInt("") == 0
+    return string.slice(0, 1).toUpperCase() + string.slice(1).toLowerCase();
 }
