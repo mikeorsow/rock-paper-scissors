cf9c165d2f5119aa9c663fbd7d24e4511d0df387
Mike Orsow <mike.orsow@gmail.com>
2022-07-27 13:39:01 -0700

    Make buttons fire playRound() and return results to console

--- script.js
+++ script.js
@@ -1,128 +1,120 @@
-// Set number of times to play the game
-//const roundsToPlay = getRoundsToPlay();
 
-// Check if the user wants to play the game
-if (roundsToPlay > 0) {
-  runGame(roundsToPlay);
-} else {
-  console.log(`Got it! No Rock, Paper, Scissors for you today!`);
-}
 
-// Run the game!
-function runGame(roundsToPlay) {
+const buttons = document.querySelectorAll("button.selection");
 
-  // Initialize Scores
-  let userScore = 0;
-  let computerScore = 0;
+buttons.forEach((button) =>
+  button.addEventListener("click", function (e) {
+    const playerChoice = this.innerText;
+    playRound(playerChoice);
+  })
+);
 
-  // Play 'roundsToPlay' number of rounds
-  for (let i = 0; i < roundsToPlay; i++) {
-    // Prompt user for their choice.
-    const promptInput = prompt("Choose Rock, Paper, or Scissors");
-
-    // Check if the user hit cancel
-    if (promptInput === null) {
-      return console.log(`Game exited!`);
-    }
-
-    // Make user choice first letter uppercase, the rest lowercase
-    const userChoice = makeFirstCharUpper(promptInput);
-
-    // Get the computer's choice.
-    const randomInt = getRandomIntBetween(0, 2);
-    const computerChoice = ["Rock", "Paper", "Scissors"][randomInt];
-
-    // Store current round number to display in round result message
-    const roundNumber = i + 1;
-
-    // Check for a tie
-    if (userChoice == computerChoice) {
-      console.log(tieMessage());
-    }
-    // If it wasn't a tie, calculate the winner of the round
-    else {
-      // See if the user or the computer won.
-      const roundWinner = getRoundWinner(userChoice, computerChoice);
-
-      // Increment the winner's score
-      roundWinner == "user" ? userScore++ : computerScore++;
-
-      // Display the round result
-      console.log(getResultMessage(roundWinner));
-    }
-
-    // ----- FUNCTIONS ----- //
-
-    // Returns a string that tells the user if they won or lost the round
-    function getResultMessage(roundWinner) {
-      return roundWinner == "user" ? winMessage() : loseMessage();
-
-      function winMessage() {
-        return `You won round ${roundNumber}! ${userChoice} beats ${computerChoice}!`;
-      }
-
-      function loseMessage() {
-        return `You lost round ${roundNumber}! ${computerChoice} beats ${userChoice}!`;
-      }
-    }
-
-    // Returns a string that tells the user it was a tie
-    function tieMessage() {
-      return `${userChoice} vs ${computerChoice}... round ${roundNumber} is a tie!`;
-    }
-
-    // Returns the winner as either 'user', or 'computer'
-    function getRoundWinner(userChoice, computerChoice) {
-      const winningCombosArr = ["paperrock", "scissorspaper", "rockscissors"];
-      const currentRoundCombo = getRoundCombo(userChoice, computerChoice);
-      const roundWinner = winningCombosArr.includes(currentRoundCombo)
-        ? "user"
-        : "computer";
-      return roundWinner;
-
-      function getRoundCombo(choice1, choice2) {
-        return `${choice1}${choice2}`.toLowerCase();
-      }
-    }
-  }
-
-  // Tell the user if they won, lost, or tied the game
-  console.log(getFinalScores());
-
-  // Returns a string with the final score of the game
-  function getFinalScores() {
-    if (userScore == computerScore) {
-      return `The game was a tie!!!!! Final Score: You: ${userScore}. CPU: ${computerScore}.`;
-    } else if (userScore > computerScore) {
-      return `You won the game!!!!! Final Score: You: ${userScore}. CPU: ${computerScore}.`;
-    } else {
-      return `You lost the game!!!!! Final Score: You: ${userScore}. CPU: ${computerScore}.`;
-    }
-  }
-}
-
-// Prompt the user to see how many times they want to play.
-// Returns an int, or 0 if the user enters a string, empty string, or hits cancel.
-function getRoundsToPlay() {
-  let promptInput = prompt(
-    "How many times would you like to play Rock Paper Scissors?",
-    5
+function playRound(playerChoice) {
+  console.log(
+    `Let's get ready to rumblllllleeeeeee! You played ${playerChoice}`
   );
 
-  // Handle cancel
-  if (promptInput === null) {
-    return (promptInput = 0);
+  // get computer choice
+  const computerChoice = getComputerChoice();
+
+  // check who won getRoundWinner - returns 'tie', 'player', 'cpu'
+  const roundWinner = getRoundWinner(playerChoice, computerChoice);
+
+  // display round result - function that accepts roundWinner, returns tie, win, lose message
+  console.log(getResultMessage(roundWinner));
+
+  function getComputerChoice() {
+    const randomInt = getRandomIntBetween(0, 2);
+    const computerChoice = ["Rock", "Paper", "Scissors"][randomInt];
+    return computerChoice;
   }
-  // By default, prompt returns a string, so let's convert to int
-  else {
-    return parseInt(promptInput);
+
+  function getResultMessage(roundWinner) {
+    if (roundWinner == "tie") {
+      return tieMessage();
+    } else if ((roundWinner = "player")) {
+      return winMessage();
+    } else {
+      return loseMessage();
+    }
+
+    function winMessage() {
+      return `You won! ${playerChoice} beats ${computerChoice}!`;
+    }
+
+    function loseMessage() {
+      return `You lost! ${computerChoice} beats ${playerChoice}!`;
+    }
+
+    function tieMessage() {
+      return `${playerChoice} vs ${computerChoice}... it's a tie!`;
+    }
   }
+
+  function getRoundWinner(playerChoice, computerChoice) {
+    const winningCombosArr = ["PaperRock", "ScissorsPaper", "RockScissors"];
+    const roundCombo = `${playerChoice}${computerChoice}`;
+
+    if (playerChoice == computerChoice) {
+      return "tie";
+    } else if (winningCombosArr.includes(roundCombo)) {
+      return "player";
+    } else {
+      return "computer";
+    }
+  }
+
+  // Returns the winner as either 'player', or 'computer'
+  
 }
+
+// ---- New Approach ----
+// playRound(playerChoice)
+// update scores
+// display current score
+// wrap everything in a while loop, while player score || cpu score is <5, show the game
+// After the while loop, we ask the user if they want to play again
+// bonus: add a 'reset' or 'start over' button that starts the game over
+
+//   // Initialize Scores
+//   let userScore = 0;
+//   let computerScore = 0;
+
+//   // Play 'roundsToPlay' number of rounds
+
+//
+//     // If it wasn't a tie, calculate the winner of the round
+//     else {
+//       // See if the user or the computer won.
+//       const roundWinner = getRoundWinner(userChoice, computerChoice);
+
+//       // Increment the winner's score
+//       roundWinner == "user" ? userScore++ : computerScore++;
+
+//       // Display the round result
+//       console.log(getResultMessage(roundWinner));
+//     }
+
+//     // ----- FUNCTIONS ----- //
+
+//     // Returns a string that tells the user if they won or lost the round
+
+//     // Returns a string that tells the user it was a tie
+
+//   // Tell the user if they won, lost, or tied the game
+//   console.log(getFinalScores());
+
+//   // Returns a string with the final score of the game
+//   function getFinalScores() {
+//     if (userScore == computerScore) {
+//       return `The game was a tie!!!!! Final Score: You: ${userScore}. CPU: ${computerScore}.`;
+//     } else if (userScore > computerScore) {
+//       return `You won the game!!!!! Final Score: You: ${userScore}. CPU: ${computerScore}.`;
+//     } else {
+//       return `You lost the game!!!!! Final Score: You: ${userScore}. CPU: ${computerScore}.`;
+//     }
+//   }
 
 function getRandomIntBetween(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
-function makeFirstCharUpper(string) {
-  return string.slice(0, 1).toUpperCase() + string.slice(1).toLowerCase();
 }
