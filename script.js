1e97daf2217307784097a528d84d8936a0c78ffa
Mike Orsow <mike.orsow@gmail.com>
2022-07-27 23:38:11 -0700

    Refactor using function expressions

--- script.js
+++ script.js
@@ -1,71 +1,41 @@
-// TO DO: Make Score Area wrap
-
-const buttons = document.querySelectorAll("button.selection");
-const resultsH2 = document.querySelector("#results h2");
-const scoreArea = document.querySelector("#score h2");
-const resultsDiv = document.querySelector("#results");
+// Create placeholder reset button for playing the game again
 const resetButton = document.createElement("button");
 
-resetButton.innerHTML = `Play Again?`;
-resetButton.classList.add("selection", "mt-20");
-resetButton.addEventListener("click", resetGame);
-resetButton.style.display = "none";
-resultsDiv.appendChild(resetButton);
+const gameButtons = document.querySelectorAll("button.selection");
 
-buttons.forEach((button) => button.addEventListener("click", clickSelection));
-
-function clickSelection(e) {
-  buttons.forEach((button) => button.classList.remove("clicked"));
+// Make buttons light up on click and trigger a round
+const clickSelection = (e) => {
+  gameButtons.forEach((button) => button.classList.remove("clicked"));
   e.target.classList.add("clicked");
-  const playerChoice = this.innerText;
+  const playerChoice = e.target.innerText;
   playRound(playerChoice);
-}
+};
 
-function resetGame() {
-  playerScore = 0;
-  computerScore = 0;
-  buttons.forEach((button) => button.classList.remove("clicked"));
-  buttons.forEach((button) => (button.style.display = "block"));
-  resultsH2.textContent = "Click to Play"
-  resetButton.style.display = "none";
-  scoreArea.textContent = "~ Best of 5 wins ~"
-}
+// Wire up the game buttons with click listeners
+gameButtons.forEach((button) =>
+  button.addEventListener("click", clickSelection)
+);
 
+// Initialize scores
 let playerScore = 0;
 let computerScore = 0;
 
 function playRound(playerChoice) {
-  // get computer choice
-  const computerChoice = getComputerChoice();
-
-  // check who won getRoundWinner - returns 'tie', 'player', 'cpu'
-  const roundWinner = getRoundWinner(playerChoice, computerChoice);
-
-  // display round result - function that accepts roundWinner, returns tie, win, lose message
-  resultsH2.textContent = getResultMessage(roundWinner);
-
-  // Increment the winner's score
-  if (roundWinner == "player") ++playerScore;
-  if (roundWinner == "computer") ++computerScore;
-
-  // update scoreboard
-  scoreArea.textContent = `You: ${playerScore}  Computer: ${computerScore}`;
-
-  // check if game is over
-  if (playerScore == 5 || computerScore == 5) {
-    resultsH2.textContent = getFinalScores();
-    buttons.forEach((button) => (button.style.display = "none"));
-    resetButton.style.display = "block";
-    scoreArea.textContent = ""
-  }
-
-  function getComputerChoice() {
+  
+  // Randomly select the computer's choice
+  const getComputerChoice = () => {
+    const getRandomIntBetween = (min, max) => {
+      return Math.floor(Math.random() * (max - min + 1) + min);
+    };
     const randomInt = getRandomIntBetween(0, 2);
     const computerChoice = ["Rock", "Paper", "Scissors"][randomInt];
     return computerChoice;
-  }
+  };
 
-  function getRoundWinner(playerChoice, computerChoice) {
+  const computerChoice = getComputerChoice();
+
+  // Calculate who won the round
+  const getRoundWinner = () => {
     const winningCombosArr = ["PaperRock", "ScissorsPaper", "RockScissors"];
     const roundCombo = `${playerChoice}${computerChoice}`;
 
@@ -76,31 +46,75 @@ function playRound(playerChoice) {
     } else {
       return "computer";
     }
-  }
+  };
 
-  function getResultMessage(roundWinner) {
+  const roundWinner = getRoundWinner();
+
+  const resultsH2 = document.querySelector("#results h2");
+
+  // Tell the user if they won, lost, or tied
+  const showRoundResultText = () => {
     if (roundWinner == "tie") {
-      return `${playerChoice} vs ${computerChoice}... it's a tie!`;
+      resultsH2.textContent = `${playerChoice} vs ${computerChoice}... it's a tie!`;
+    } else if (roundWinner == "player") {
+      resultsH2.textContent = `You won! ${playerChoice} beats ${computerChoice}!`;
+    } else if (roundWinner == "computer") {
+      resultsH2.textContent = `You lost! ${computerChoice} beats ${playerChoice}!`;
     }
-    if (roundWinner == "player") {
-      return `You won! ${playerChoice} beats ${computerChoice}!`;
-    }
-    if (roundWinner == "computer") {
-      return `You lost! ${computerChoice} beats ${playerChoice}!`;
-    }
-  }
+  };
 
-  function getFinalScores() {
+  showRoundResultText();
+
+  const scoreH2 = document.querySelector("#score h2");
+
+  const showUpdatedScores = () => {
+    if (roundWinner == "player") ++playerScore;
+    if (roundWinner == "computer") ++computerScore;
+
+    scoreH2.textContent = `You: ${playerScore}  Computer: ${computerScore}`;
+  };
+
+  showUpdatedScores();
+
+  const showFinalScore = () => {
     if (playerScore > computerScore) {
-      return `You won the game!!!!!
+      resultsH2.innerHTML = `You won the game! <br /> 
       Final Score: You: ${playerScore}. CPU: ${computerScore}.`;
     } else {
-      return `You lost the game!!!!!
+      resultsH2.innerHTML = `You lost the game! <br />
       Final Score: You: ${playerScore}. CPU: ${computerScore}.`;
     }
-  }
+  };
 
-  function getRandomIntBetween(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
+  const resetGame = () => {
+    playerScore = 0;
+    computerScore = 0;
+    gameButtons.forEach((button) => button.classList.remove("clicked"));
+    gameButtons.forEach((button) => (button.style.display = "block"));
+    resultsH2.textContent = "Click to Play";
+    resetButton.style.display = "none";
+    scoreH2.textContent = "~ Best of 5 wins ~";
+  };
+
+  const showResetButton = () => {
+    const resultsDiv = document.querySelector("#results");
+    resetButton.innerHTML = `Play Again?`;
+    resetButton.classList.add("selection", "mt-20");
+    resetButton.addEventListener("click", resetGame);
+    resetButton.style.display = "block";
+    resultsDiv.appendChild(resetButton);
+  };
+
+  const clearRoundText = () => {
+    gameButtons.forEach((button) => (button.style.display = "none"));
+    scoreH2.textContent = "";
+  };
+
+  const gameIsOver = playerScore == 5 || computerScore == 5;
+
+  if (gameIsOver) {
+    showFinalScore();
+    clearRoundText();
+    showResetButton();
   }
 }
